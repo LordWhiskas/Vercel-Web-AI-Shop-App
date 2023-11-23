e9cf57e8547e6d7f0721c254b00be5614dc4b722
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-11-23 22:45:44 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,35 +9,21 @@ function App() {
     setProducts("lalala");
   }, []);
 
-  const [category, setCategory] = useState('all');
-
-// Add a function to update the category
-//   const filterByCategory = (newCategory) => {
-//     setCategory(newCategory);
-//   };
-
-// Adjust your useEffect to fetch products based on the selected category
-  useEffect(() => {
-    // Fetch products from your backend API based on category
-  }, [category]);
-
-// Now, filter the products before rendering
-//   const filteredProducts = category === 'all' ? products : products.filter(product => product.category === category);
-
   return (
       <div className="product-grid">
-        {products.map((product) => (
-            <div key={product.id} className="product-card">
-              <img src={product.image} alt={product.name} />
-              <div className="product-info">
-                <h3>{product.name}</h3>
-                <p>{product.description}</p>
-                <p>${product.price}</p>
-                {/* Add to Cart Button */}
-                <button>Add to Cart</button>
-              </div>
-            </div>
-        ))}
+        Lalala
+        {/*{products.map((product) => (*/}
+        {/*    <div key={product.id} className="product-card">*/}
+        {/*      <img src={product.image} alt={product.name} />*/}
+        {/*      <div className="product-info">*/}
+        {/*        <h3>{product.name}</h3>*/}
+        {/*        <p>{product.description}</p>*/}
+        {/*        <p>${product.price}</p>*/}
+        {/*        /!* Add to Cart Button *!/*/}
+        {/*        <button>Add to Cart</button>*/}
+        {/*      </div>*/}
+        {/*    </div>*/}
+        {/*))}*/}
       </div>
   );
 }
