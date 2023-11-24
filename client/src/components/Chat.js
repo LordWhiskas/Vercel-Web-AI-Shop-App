41186133fd2157b725ea7ea215161b7032125128
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-11-24 19:36:38 +0100

    Revert changes

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,12 +1,9 @@
-import React, { useState, useContext } from 'react';
-import { CategoryContext } from './CategoryContext'; // Import the context you created
+import React, { useState } from 'react';
 import '../styles/Chat.css';
 
-const Chat = ({ onSendMessage }) => {
+const Chat = ({ onSendMessage, messages }) => {
     const [userInput, setUserInput] = useState('');
     const [isSending, setIsSending] = useState(false);
-    const { setCategory } = useContext(CategoryContext); // Use the context to set the selected category
-    const [messages, setMessages] = useState([]);
 
     const handleInputChange = (e) => {
         setUserInput(e.target.value);
@@ -15,15 +12,10 @@ const Chat = ({ onSendMessage }) => {
     const handleSend = async () => {
         if (userInput.trim()) {
             setIsSending(true);
-            const response = await onSendMessage(userInput);
+            await onSendMessage(userInput);
             setIsSending(false);
             setUserInput(''); // Clear the input after sending
-            setMessages([...messages, { role: 'user', content: userInput }, { role: 'assistant', content: response }]);
         }
-    };
-
-    const handleCategorySelect = (category) => {
-        setCategory(category); // Set the selected category in the context
     };
 
     return (
@@ -32,16 +24,6 @@ const Chat = ({ onSendMessage }) => {
                 {messages.map((msg, index) => (
                     <div key={index} className={`message ${msg.role}`}>
                         {msg.content}
-                        {/* If the message is from the assistant and contains categories, render buttons */}
-                        {msg.role === 'assistant' && msg.content.includes('Categories:') && (
-                            <div className="category-buttons-container">
-                                {msg.content.match(/Categories:\s\[(.*?)\]/)[1].split(', ').map((category, idx) => (
-                                    <button key={idx} onClick={() => handleCategorySelect(category)} className="category-button">
-                                        {category}
-                                    </button>
-                                ))}
-                            </div>
-                        )}
                     </div>
                 ))}
                 {isSending && <div className="typing-indicator"></div>}
