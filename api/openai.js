360cb6d9205b9fa8726ed236d8151f79b140d639
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-11-24 16:15:14 +0100

    OpenAI Assistant added-Fixing-8

--- api/openai.js
+++ api/openai.js
@@ -8,27 +8,37 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY);
 const responseHandler = async (req, res) => {
     try {
         const thread = await openai.beta.threads.create();
-        const message = await openai.beta.threads.messages.create(thread.id, {
+        await openai.beta.threads.messages.create(thread.id, {
             role: "user",
             content: req.body.content // Assuming the message is sent in the body of the request
         });
 
         const run = await openai.beta.threads.runs.create(thread.id, {
-            assistant_id: "asst_4IjSi0qdqrSse3tD6PmjdDK9", // use your assistant ID here
+            assistant_id: "asst_4IjSi0qdqrSse3tD6PmjdDK9", // Use your assistant ID here
         });
 
-        let run_retrieve = await openai.beta.threads.runs.retrieve(thread.id, run.id);
-        // Consider a non-blocking approach instead of while loop here
+        // Instead of while loop, use a recursive function with a timeout
+        const getRunResult = async (threadId, runId) => {
+            const result = await openai.beta.threads.runs.retrieve(threadId, runId);
+            if (result.status === 'completed') {
+                return result;
+            } else {
+                // Delay the next call by a certain amount of time (e.g., 500ms)
+                await new Promise(resolve => setTimeout(resolve, 500));
+                return getRunResult(threadId, runId); // Recurse until completed
+            }
+        };
 
-        const messages = await openai.beta.threads.messages.list(thread.id);
-        const result = messages.body.data; // Get the last message
+        const completedRun = await getRunResult(thread.id, run.id);
 
-        res.json(messages); // Send the result back as JSON
+        // Assuming the last message in the thread is the assistant's response
+        const assistantMessage = completedRun.messages[completedRun.messages.length - 1];
+
+        res.json({ response: assistantMessage.content.text });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Error processing your request' });
     }
 };
-
 
 export default responseHandler;
