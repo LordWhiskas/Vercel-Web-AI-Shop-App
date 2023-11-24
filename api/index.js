e936ea88fb0e887de94bb2c36a427903b5f79825
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-11-24 02:12:34 +0100

    MongoDB connection-1

--- api/index.js
+++ api/index.js
@@ -16,22 +16,19 @@ const client = new MongoClient(uri, {
     }
 });
 
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+export default async function handler(req, res) {
+    async function run() {
+        try {
+            // Connect the client to the server	(optional starting in v4.7)
+            await client.connect();
+            // Send a ping to confirm a successful connection
+            await client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+        }
     }
+    run().catch(console.dir);
+    res.json(200);
 }
-run().catch(console.dir);
-
-app.get("/api/hello/", (req, res) => {
-    res.json({
-        message: "Hello World"
-    });
-});
