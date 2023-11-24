31a2471f100d6e312f8db8284e060f5475add786
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-11-24 02:08:44 +0100

    MongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,34 @@
 import Express  from "express";
 import cors from "cors";
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = Express();
+require('dotenv').config();
 
 app.use(cors());
+
+const uri = process.env.MONGODB
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get("/api/hello/", (req, res) => {
     res.json({
