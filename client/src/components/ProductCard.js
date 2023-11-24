d9fcec469c2d73e4cc830e147bc45b747ad4f586
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-11-24 17:48:21 +0100

    Update image path

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -3,9 +3,10 @@ import React from 'react';
 import '../styles/ProductCard.css';
 
 function ProductCard({ product, addToCart }) {
+    const imageUrl = require(`../assets/${product.image}`).default;
     return (
         <div className="product-card">
-            <img src={product.image} alt={product.name} />
+            <img src={imageUrl} alt={product.name} />
             <h3>{product.name}</h3>
             <p>${product.price}</p>
             <button onClick={() => addToCart(product)}>Add to Cart</button>
