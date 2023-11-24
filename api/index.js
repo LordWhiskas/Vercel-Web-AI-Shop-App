ff907abc4b7a969c042b1398c761d7ae4dbd86e5
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-11-24 01:42:39 +0100

    Implement file-based routing for API endpoints

--- api/index.js
+++ api/index.js
@@ -1,19 +1,17 @@
 import Express  from "express";
 import cors from "cors";
-
+const {readdirSync} = require("fs")
 const app = Express();
 
 app.use(cors());
+
+readdirSync("./api").map((file)=>app.use("/",require("./api/"+file)))
 
 app.get("/api/hello/", (req, res) => {
     res.json({
         message: "Hello World"
     });
 });
-
-// app.get('/api/products', (req, res) => {
-//     res.json(products);
-// });
 
 app.listen(8000, () => {
     console.log("Server running on port 8000");
